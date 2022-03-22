75cd4915ae546a9912614d0970544e16f1eb1ee8
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-22 23:26:16 +0600

    add to cart button event handler

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,6 @@ import './Product.css'
 const Product = (props) => {
   const {name, img, seller, price, ratings} = props.product;
 
-
   return (
     <div className='product'>
       <img src={img} alt="" />
@@ -15,7 +14,7 @@ const Product = (props) => {
         <p><small>Ratings: {ratings}</small></p>
     </div>
     <button className='btn-cart'>
-      <p>Add to Cart</p>
+      <p onClick={() => props.handleAddToCart(props.product)}>Add to Cart</p>
     </button>
     </div>
   );
