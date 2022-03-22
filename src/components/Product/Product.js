65ab3a3bc8b106fce35ac3511792aa20b6274492
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-22 22:54:29 +0600

    added product info & add to to cart button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-  const {name, img, seller, price, retings} = props.product;
+  const {name, img, seller, price, ratings} = props.product;
 
 
   return (
     <div className='product'>
       <img src={img} alt="" />
-      <p className='product-name'>{name}</p>
+    <div className="product-info">
+        <p className='product-name'>{name}</p>
+        <p>Price: ${price}</p>
+        <p><small>Seller: {seller}</small></p>
+        <p><small>Ratings: {ratings}</small></p>
+    </div>
+    <button className='btn-cart'>
+      <p>Add to Cart</p>
+    </button>
     </div>
   );
 };
