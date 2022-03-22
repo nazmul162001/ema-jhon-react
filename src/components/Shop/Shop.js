f3770f8d0f7f6f6424b38ab15b3c54ca8bfbf7cf
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-22 21:53:46 +0600

    add json & fetched product

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+
+const Shop = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('products.json')
+    .then(res => res.json())
+    .then(data =>setProducts(data))
+  } ,[])
+  return (
+    <div className='shop-container'>
+      <div className="products-container">
+        <h3>This is products: {products.length} </h3>
+      </div>
+      <div className="cart-container">
+        <h4>Order summery</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
