b5dd86725836bda208c93330c02a1fccc2576bf7
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-23 16:08:30 +0600

    update total price , update shipping fee, update tax & update grand total price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+  const {cart} = props;
+  // add total price // total shipping price
+  let total = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  // tax 
+  const tax = +(total * 0.1).toFixed(2);
+  // grand Total
+  const grandTotal = (total + shipping + tax).toFixed(2);
+
   return (
     <div className='cart'>
-          <h2>Order summery</h2>
+          <h4>Order summery</h4>
           <p>Selected Items: {cart.length}</p>
+          <p>Total Price: ${total} </p>
+          <p>Total Shipping: ${shipping}</p>
+          <p>Tax: {tax} </p>
+          <h5> Grand Total: ${grandTotal}</h5>
     </div>
   );
 };
