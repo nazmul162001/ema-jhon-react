6c293f15715331a359d1b31758ed03e2d4b97a52
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-31 11:59:40 +0600

    create custom hook for shopping cart

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProduct';
 
 const Orders = () => {
   const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
   return (
     <div>
       <h2>This is orders {products.length} </h2>
+      <p>Cart has: {cart.length}</p>
     </div>
   );
 };
