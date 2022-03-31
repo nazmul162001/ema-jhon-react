515b6f60c9a364ec4b500253608c955d96e6a220
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-31 13:04:40 +0600

    added product review page styled

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -3,13 +3,14 @@ import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProduct';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import './Orders.css'
 
 const Orders = () => {
   const [products, setProducts] = useProducts();
   const [cart, setCart] = useCart(products);
   return (
     <div className='shop-container'>
-      <div className="products-container">
+      <div className="review-items-container">
         {
           cart.map(product => <ReviewItem 
           key = {product.id}
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './ReviewItem.css'
 
 const ReviewItem = (props) => {
-  const {name, price, shipping, quantity} = props.product;
+  const {name, img, price, shipping, quantity} = props.product;
   return (
-    <div>
-      <h3>Name: {name}</h3>
+    <div className='review-item'>
+      <div>
+        <img src= {img} alt="" />
+      </div>
+      <div className='review-items-details-container'>
+          <div className="review-item-details">
+            <p className='product-name' title={name}>{name.length > 20 ? name.slice(0, 20) + '...' : name}</p>
+            <p className="price">Price: <span className='orange-color'>{price}</span></p>
+            <p className="shipping"><small className="shipping-price">{shipping}</small></p>
+            <p><small>{quantity}</small></p>
+          </div>
+          <div className="delete-container">
+            <button>Delete</button>
+          </div>
+      </div>
     </div>
   );
 };
