84f5e24ac75d59e2589188a9961b28ac20cb0c3b
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-10 16:44:13 +0600

    deploy site to firebase

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -18,8 +18,9 @@
 
 /**
  * firebase hosting steps
- * 1. npm install -g firebase-tools
- * 2. firebase login
- * 3. firebase init
- * 4. firebase deploy
+ * 1. npm install -g firebase-tools (one time for your computer)
+ * 2. firebase login (one time for your computer)
+ * 3. firebase init (each project)
+ * 4. npm run build (deploy your project) (every time you want to build)
+ * 5. firebase deploy (each project)
  */
