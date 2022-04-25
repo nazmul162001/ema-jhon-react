0b9a73ca3742ac0dcdf334d550de72e12ebafb56
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-25 06:52:24 +0600

    connect mongodb & load data from server & also added pagination

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,56 +1,72 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useProducts from '../../hooks/useProduct';
-import { addToDb, getStoredCart } from '../../utilities/fakedb'
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
   const [products, setProducts] = useProducts();
-  // use state for handle click 
+  // use state for handle click
   const [cart, setCart] = useState([]);
+  const [pageCount, setPageCount]= useState(0)
 
-  // set LocalStorage 
+  useEffect(() => {
+    fetch('http://localhost:5000/productCount')
+    .then(res => res.json())
+    .then(data => {
+      const count = data.count;
+      const pages = Math.ceil(count/10)
+      setPageCount(pages)
+    })
+  },[])
+
+  // set LocalStorage
   useEffect(() => {
     const storedCart = getStoredCart();
     const savedCart = [];
-    for(const id in storedCart){
-      const addedProduct = products.find(product => product.id === id);
-      if(addedProduct){
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product._id === id);
+      if (addedProduct) {
         const quantity = storedCart[id];
         addedProduct.quantity = quantity;
         savedCart.push(addedProduct);
       }
     }
     setCart(savedCart);
-  },[products]);
-  
-
-
+  }, [products]);
 
   const handleAddToCart = (product) => {
     console.log(product);
     const newCart = [...cart, product];
     setCart(newCart);
-    addToDb(product.id)
-  }
+    addToDb(product._id);
+  };
 
   return (
-    <div className='shop-container'>
+    <div className="shop-container">
       <div className="products-container">
-        {
-          products.map(product => <Product
-          key = {product.id}
-          product = {product}
-          handleAddToCart = {handleAddToCart}
+        {products.map((product) => (
+          <Product
+            key={product._id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
 
-          ></Product>)
-        }
+          <div className='pagination'>
+          {
+            [...Array(pageCount).keys()]
+            .map(number => <button> {number} </button>)
+          }
+          </div>
+
+
       </div>
       <div className="cart-container">
-        <Cart cart = {cart}>
-          <Link to='/orders'>
+        <Cart cart={cart}>
+          <Link to="/orders">
             <button>Review Order</button>
           </Link>
         </Cart>
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import { getStoredCart } from "../utilities/fakedb";
+import { useEffect, useState } from 'react';
+import { getStoredCart } from '../utilities/fakedb';
 
 const useCart = (products) => {
   const [cart, setCart] = useState([]);
@@ -7,18 +7,18 @@ const useCart = (products) => {
   useEffect(() => {
     const storedCart = getStoredCart();
     const savedCart = [];
-    for(const id in storedCart) {
-      const addedProduct = products.find(product => product.id === id);
-      if(addedProduct){
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product._id === id);
+      if (addedProduct) {
         const quantity = storedCart[id];
         addedProduct.quantity = quantity;
         savedCart.push(addedProduct);
       }
     }
     setCart(savedCart);
-  },[products]);
+  }, [products]);
 
   return [cart, setCart];
-}
+};
 
 export default useCart;
